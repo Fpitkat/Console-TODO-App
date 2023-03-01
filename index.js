f64798c76a249e0de8f85b4db2b635166fa191e8
Frank Pitkat <fpitkat@gmail.com>
2023-03-01 14:13:57 -0500

    final commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const todos = ['collect chicken eggs', 'clean litter boxes']
+
+let input = prompt('What would you like todo?')
+
+while (input !== 'quit' && input !== 'q') {
+  if (input === 'list') {
+    console.log('************')
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`)
+    }
+    console.log('************')
+  } else if (input === 'new') {
+    const newTodo = prompt('What would you like add?')
+    todos.push(newTodo)
+    console.log(`${newTodo} added to your list`)
+  } else if (input === 'delete') {
+    const index = parseInt(prompt('What index to delete?'))
+
+    if (Number.isNaN(index)) {
+      console.log('Please enter a valid index')
+    } else {
+      const deleted = todos.splice(index, 1)
+      console.log(`${deleted} has been removed from your list`)
+    }
+  }
+  input = prompt('What would you like todo?')
+}
+
+console.log('You quit the app')
